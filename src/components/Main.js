e5c1776394a8e1747088362e51790e53a35249e8
Gabe Amaya <gabeamaya2@gmail.com>
2019-11-26 12:25:57 -0600

    update styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,16 @@
 import React from "react"
 import Link from "gatsby-link"
-import Layout from "../components/Layout"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 
 class Main extends React.Component {
   render() {
     return (
       <main>
-        <Layout>
-          <Hero />
-          <Experience />
-          <Approach />
-          <Software />
-        </Layout>
+        <Hero />
+        <Experience />
+        <Approach />
+        <Software />
       </main>
     )
   }
@@ -19,26 +18,28 @@ class Main extends React.Component {
 
 const Hero = () => (
   <section className="hero">
-    <div className="membersy_grid--wrapper">
-      <div className="membersy_grid--item">
-        <h1>membership dentistry has arrived</h1>
-        <p>
-          Today's consumers have come to expect a highly personal and connected
-          experience in everything they do. It's time denistry met those
-          expectations.
-        </p>
-        <div className="btn_wrapper">
-          <button className="membersy_btn--primary">
-            <Link to="/">schedule free consultation</Link>
-          </button>
-          <button className="membersy_btn--secondary">
-            <Link to="/">learn more</Link>
-          </button>
+    <div className="container">
+      <div className="membersy_grid--wrapper">
+        <div className="membersy_grid--item">
+          <h1 className="membersy_heading">membership dentistry has arrived</h1>
+          <p>
+            Today's consumers have come to expect a highly personal and
+            connected experience in everything they do. It's time denistry met
+            those expectations.
+          </p>
+          <div className="btn_wrapper">
+            <button className="membersy_btn--primary">
+              <Link to="/">schedule free consultation</Link>
+            </button>
+            <button className="membersy_btn--secondary">
+              <Link to="/">learn more</Link>
+            </button>
+          </div>
         </div>
-      </div>
 
-      <div className="membersy_grid--item">
-        <img src="https://via.placeholder.com/150" alt="placeholder" />
+        <div className="membersy_grid--item">
+          <img src="https://via.placeholder.com/150" alt="placeholder" />
+        </div>
       </div>
     </div>
   </section>
@@ -46,102 +47,136 @@ const Hero = () => (
 
 const Experience = () => (
   <section className="experience background_gray">
-    <h2>a membership experience</h2>
-    <p>
-      We're membersy, an Austin, Texas-based digital health company that prvieds
-      dental practices with thoughtfully designed membership programs to make
-      dental care more approachable.
-    </p>
-    <div className="mac_box--wrapper">
-      <img src="https://via.placeholder.com/100x100" alt="placeholder" />
-      <h4>
-        welcome to <span className="color_membersy">membersy</span>
-      </h4>
+    <div className="container u_text--center">
+      <h2 className="membersy_subheading">a membership experience</h2>
       <p>
-        Discover how our advanced membership solutions can help you launch and
-        grow your own proprietary, regulatory compliant and professionally
-        administered dental membership program.
+        We're membersy, an Austin, Texas-based digital health company that
+        prvieds dental practices with thoughtfully designed membership programs
+        to make dental care more approachable.
       </p>
+      <div className="mac_box--wrapper">
+        <img src="https://via.placeholder.com/100x100" alt="placeholder" />
+        <div className="u_flex--text">
+        <h4>
+          welcome to <span className="color_membersy">membersy</span>
+        </h4>
+        <p>
+          Discover how our advanced membership solutions can help you launch and
+          grow your own proprietary, regulatory compliant and professionally
+          administered dental membership program.
+        </p>
+        </div>
+      </div>
+      <Link to="/">learn more about dental membership plans</Link>
     </div>
-    <Link to="/">learn more about dental membership plans</Link>
   </section>
 )
 
 const Approach = () => (
   <section className="approach">
-    <div className="membersy_grid--wrapper">
-      <div className="membersy_grid--item">
-        <p className="membersy_badge">how it works</p>
-        <h2>our approach</h2>
-        <p>
-          Whether you currently have a membership program in place and are
-          looking to improve it, or you would like a new one built from scratch,
-          our team is here to help.
-        </p>
-      </div>
-      <div className="membersy_grid--item">
-        <img src="https://via.placeholder.com/100x100" alt="placeholder" />
-      </div>
-    </div>
-    <div className="membersy_grid--wrapper">
-      <div className="membersy_grid--item">
-        <div className="membersy_card">
-          <p className="number_badge">1</p>
-          <h4>design</h4>
+    <div className="container">
+      <div className="membersy_grid--wrapper">
+        <div className="membersy_grid--item">
+          <p className="membersy_badge">how it works</p>
+          <h2 className="membersy_subheading">our approach</h2>
           <p>
-            <em>with you from day one --</em> We’ll evaluate your current
-            self-pay patient workflow (or lack thereof) and provide data-driven
-            guidance on how to effectively structure, promote and sell your new
-            or revamped membership program so the end result is an increase to
-            your case acceptance rates, services rendered and patient loyalty.
+            Whether you currently have a membership program in place and are
+            looking to improve it, or you would like a new one built from
+            scratch, our team is here to help.
           </p>
         </div>
-      </div>
-      <div className="membersy_grid--item">
-        <div className="membersy_card">
-          <p className="number_badge">2</p>
-          <h4>develop</h4>
-          <p>
-            <em>when things get real --</em> We’ll evaluate your current
-            self-pay patient workflow (or lack thereof) and provide data-driven
-            guidance on how to effectively structure, promote and sell your new
-            or revamped membership program so the end result is an increase to
-            your case acceptance rates, services rendered and patient loyalty.
-          </p>
+        <div className="membersy_grid--item">
+          <img src="https://via.placeholder.com/100x100" alt="placeholder" />
         </div>
       </div>
-      <div className="membersy_grid--item">
-        <div className="membersy_card">
-          <p className="number_badge">3</p>
-          <h4>administer</h4>
-          <p>
-            <em>hands-on support --</em> We’ll evaluate your current self-pay
-            patient workflow (or lack thereof) and provide data-driven guidance
-            on how to effectively structure, promote and sell your new or
-            revamped membership program so the end result is an increase to your
-            case acceptance rates, services rendered and patient loyalty.
-          </p>
+      <div className="membersy_grid--wrapper u_spacing--top">
+        <div className="membersy_grid--item-3">
+          <div className="membersy_card">
+            <div className="number_badge">1</div>
+            <h4>design</h4>
+            <p>
+              <em>with you from day one --</em> We’ll evaluate your current
+              self-pay patient workflow (or lack thereof) and provide
+              data-driven guidance on how to effectively structure, promote and
+              sell your new or revamped membership program so the end result is
+              an increase to your case acceptance rates, services rendered and
+              patient loyalty.
+            </p>
+          </div>
+        </div>
+        <div className="membersy_grid--item-3">
+          <div className="membersy_card">
+            <div className="number_badge">2</div>
+            <h4>develop</h4>
+            <p>
+              <em>when things get real --</em> We’ll evaluate your current
+              self-pay patient workflow (or lack thereof) and provide
+              data-driven guidance on how to effectively structure, promote and
+              sell your new or revamped membership program so the end result is
+              an increase to your case acceptance rates, services rendered and
+              patient loyalty.
+            </p>
+          </div>
+        </div>
+        <div className="membersy_grid--item-3">
+          <div className="membersy_card">
+            <div className="number_badge">3</div>
+            <h4>administer</h4>
+            <p>
+              <em>hands-on support --</em> We’ll evaluate your current self-pay
+              patient workflow (or lack thereof) and provide data-driven
+              guidance on how to effectively structure, promote and sell your
+              new or revamped membership program so the end result is an
+              increase to your case acceptance rates, services rendered and
+              patient loyalty.
+            </p>
+          </div>
         </div>
       </div>
     </div>
   </section>
 )
 
-const Software = () => (
+const Software = ({ data }) => (
   <section className="software background_gray">
-    <div className="membersy_grid--wrapper">
-      <div className="membersy_grid--item">
-      <p className="membersy_badge">dental membership software</p>
-    <h2>power your membership program with cutting-edge software.</h2>
-    <p>Bring your community together with a simple, intuitive membership dashboard that automates your member workflow and scales as your member base grows.</p>
-    <p>Our robust membership software features an online dashboard that displays real-time KPI's vital to the health of your membership programs.</p>
-    <button className="membersy_btn">learn more</button>
-      </div>
-      <div className="membersy_grid--item">
-      <img src="https://via.placeholder.com/250x200" alt="placeholder" />
+    <div className="container">
+      <div className="membersy_grid--wrapper">
+        <div className="membersy_grid--item">
+          <p className="membersy_badge">dental membership software</p>
+          <h2 className="membersy_subheading">power your membership program with cutting-edge software.</h2>
+          <p>
+            Bring your community together with a simple, intuitive membership
+            dashboard that automates your member workflow and scales as your
+            member base grows.
+          </p>
+          <p>
+            Our robust membership software features an online dashboard that
+            displays real-time KPI's vital to the health of your membership
+            programs.
+          </p>
+          <button className="membersy_btn u_spacing--top-small">learn more</button>
+        </div>
+        <div className="membersy_grid--item">
+          {/* <Img fixed={data.file.childImageSharp.fixed} /> */}
+          <img src="https://via.placeholder.com/250x200" alt="placeholder" />
+        </div>
       </div>
     </div>
   </section>
 )
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "/dash-components.jpg" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fixed(width: 125, height: 125) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
 
 export default Main
